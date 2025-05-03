cbd99bd32e7870dc49a96b824a1a82fcae197d02
Mikita <ghjhjr11@gmail.com>
2025-05-03 23:30:36 +0300

    2

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-async function fetchData<T>(url: string) {
+async function fetchData<T>(url: string): Promise<T> {
   try {
     const response = await axios.get<T>(url);
     return response.data;
--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,3 +1,3 @@
-function merge<A extends Object, B extends Object>(objA: A, objB: B) {
+function merge<A extends Object, B extends Object>(objA: A, objB: B): A & B {
   return Object.assign(objA, objB);
 }
